af66f1773390b61101d3eaffb76a2d9f5852e975
Vinícius <vfmns@cesar.school>
2026-09-05 20:34:19 +0000

    Remove idle reset in src.c
    Remove idle reset when top is not NULL.

--- src.c
+++ src.c
@@ -47,7 +47,6 @@ task *org(task *head, task *top, unsigned int count){
       if(re->next == top){
         if(top != NULL){
           fprintf(fptr, "[%s] for %u units - H\n", top->name, extime);
-          idle = 0;
         }
         top = re;
         extime = 0;
